6289471aa6ecfc894cb9c7ce138e7f464560b544
Zellux Wang <zellux@gmail.com>
2017-03-26 23:04:41 -0700

    Fancybox shortcode

--- static/js/slim.js
+++ static/js/slim.js
@@ -2,3 +2,7 @@ document.querySelector('.nav-btn').onclick = function(e) {
    e.preventDefault();
    this.classList.toggle('is-on');
 };
+
+$(document).ready(function() {
+	$(".fancybox").fancybox();
+});
